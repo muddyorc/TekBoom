61be0fe1db428433fc30dd2949e6557b1a969adb
Julio Cezar <juliocezarrc16@gmail.com>
2025-04-05 16:31:46 -0300

    create-account

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,58 @@ export default async function Home() {
     if(loggedIn) redirect("/user-app", RedirectType.replace);
   }
 
-  return <div className="flex flex-col h-screen w-full justify-center item-center">
-    
+  return <div className="flex flex-col h-screen w-full justify-center items-center">
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="create-account">Account</TabsTrigger>
+        <TabsTrigger value="login">Login</TabsTrigger>
+      </TabsList> 
+      <TabsContent value="create-account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Create Your Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue="" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="Password">Password</Label>
+              <Input id="current" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="login">
+        <Card>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue="" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">Password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
   </div>
 }
